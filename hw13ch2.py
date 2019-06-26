da53325f6a2e48321efcdbb0a78ee1cf69b727e4
Hanee01 <chigolls23@gmail.com>
2019-06-26 19:35:59 +0600

    lol

---
+++ hw13ch2.py
@@ -0,0 +1,8 @@
+# 13. Given a string consisting of exactly two words separated by a space.
+# Print a new string with the first and second word positions swapped (the second word is printed first).
+# This task should not use loops and if.
+
+
+wordA = str(input("Enter word A: "))
+wordB = str(input("Enter word B: "))
+print(wordB + " " + wordA)
